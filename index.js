f31a244d556daea4572aef2fda3e6f41a0868348
Clsrfish <clsrfish@gmail.com>
2022-03-27 01:39:51 +0800

    opt: stop rendering when accessing invalid file detected

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
 // replace relative markdown image path with absolute path to hexo root
 const path = require('path');
+const fs = require('fs');
+
 let imageReg = /!\[(?<alt>[^\]]*)\]\((?<url>.*?)(?=\"|\))(?<title>\".*\")?\)/g;
 let cwd = process.cwd();
 
-let hexo_img_locator = function (data) {
+function printInfo(msg) {
+    hexo.log.info(msg)
+}
 
+function printError(msg) {
+    hexo.log.e(msg)
+}
+
+let hexo_img_locator = function (data) {
     let postPath = data.full_source;
     if (!postPath.endsWith('.md')) {
-        console.log(`skip non-md file: ${postPath}`);
+        printInfo(`skip ${postPath}`);
+        return data;
     }
-    let substitution = new Map();
+    let mapping = new Map();
+    let cannotAccess = new Array();
+
     let match = null;
     while ((match = imageReg.exec(data.content)) != null) {
+        // skip online image and local file with absolute path
         let imageUrl = match.groups.url;
-        if (imageUrl.startsWith('http://') || imageUrl.startsWith('https://')) {
-            console.log(`skip online image: ${imageUrl}`);
+        if (imageUrl.startsWith('http://')
+            || imageUrl.startsWith('https://')
+            || imageUrl.startsWith('/')) {
             continue;
         }
-        if (imageUrl.startsWith('/')) {
-            console.log(`skip absolute path: ${imageUrl}`);
+        let imageAbsPath = path.resolve(path.dirname(postPath), imageUrl);
+
+        if (!fs.existsSync(imageAbsPath)) {
+            cannotAccess.push(imageUrl);
+            continue;
+        } else if (cannotAccess.length > 0) {
+            // no need process any more
             continue;
         }
-        let imageSytax = match[0];
-        let imagePath = path.resolve(path.dirname(postPath), imageUrl);
-        substitution.set(imageSytax,
-            imageSytax.replace(imageUrl, imagePath.replace(`${cwd}`, ''))
-                .replace('/source', ''));
+
+        let imageAbsPathFromSource = path.sep + path.relative(`${cwd}${path.sep}source`, imageAbsPath);
+        let imageDirective = match[0];
+        mapping.set(imageDirective,
+            imageDirective.replace(imageUrl, imageAbsPathFromSource));
+        printInfo(`[hexo-img-locator] ${imageUrl} -> ${imageAbsPathFromSource}`);
     }
-    substitution.forEach((v, k, map) => {
-        console.info(`substituting ${k} -> ${v}`);
+
+    if (cannotAccess.length != 0) {
+        printError(`[hexo-img-locator] cannot access ${cannotAccess[0]} and other ${cannotAccess.length - 1} from ${path.relative(cwd, data.full_source)}, file not found.`)
+        process.exit(1)
+    }
+    mapping.forEach((v, k, map) => {
         data.content = data.content.replace(k, v);
     })
     return data;
